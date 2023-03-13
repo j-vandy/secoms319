5ace7296d5278993caf7ff818a0e55f67bf21090
jvandy <jacksonvan2003@gmail.com>
2023-03-12 20:11:18 -0500

    Fixed the code that someone broke
    goblin.

--- index.js
+++ index.js
@@ -110,8 +110,8 @@ function goAboutUs() {
   return false;
 }
 function goThird() {
-  document.getElementById("third").style.display = 'block';
+  document.getElementById("home").style.display = 'none';
   document.getElementById("aboutus").style.display = 'none';
-  document.getElementById("studentinfo").style.display = 'none';
+  document.getElementById("studentinfo").style.display = 'block';
   return false;
 }
